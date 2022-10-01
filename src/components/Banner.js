d59960c155b2cde249aad35186d3a9ae78629525
cDev-Creator <mccoyng170@gmail.com>
2022-10-01 10:42:23 -0400

    basic banner markup and styling

---
+++ src/components/Banner.js
@@ -0,0 +1,28 @@
+import {Container, Row, Col} from "react-bootstrap";
+import {ArrowRightCircle} from "react-bootstrap-icons";
+import header from "../assets/img/nav1.png";
+
+
+export const Banner = () => {
+    return (
+        <section className='banner' id='home'>
+            <Container>
+                <Row className='align-items-center'>
+                    <Col xs={12} md={6} xl={7}>
+                        <span className='tagline'>Welcome to my Portfolio</span>
+                        <h1>{'Hi I am Natalie McCoy'}'<span className='wrap'>webdeveloper</span></h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <button onclick={()=> console.log('connect')}>Let's Connect<ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={header} alt='Header'></img>
+
+                    </Col>
+                </Row>
+
+            </Container>
+
+
+        </section>
+    )
+}
