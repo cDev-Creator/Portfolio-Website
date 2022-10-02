7f8d86d2590dc5c7db1c81d7a724a5dc04ba3a2a
cDev-Creator <mccoyng170@gmail.com>
2022-10-02 11:27:13 -0400

    basic footer structure

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 
+import { Footer } from "./components/Footer";
+
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Banner />
       <Skills /> 
       <Projects />
+
+      <Footer />
     </div>
   );
 }
